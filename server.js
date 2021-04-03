f355596e6fe8d4965df807cbb55f1871ffa8e571
Mr. Llanes <llanesm@gmail.com>
2021-04-03 09:58:58 -0500

    Heroku deploy part 2.

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const PORT = process.env.PORT || 3000;
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
+	useCreateIndex: true,
+	useFindAndModify: false,
 });
 
 app.use(logger("dev"));
